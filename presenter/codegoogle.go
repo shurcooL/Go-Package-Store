e512584d7b0d1d3e69f42bbcfd96ecb378222c98
Dmitri Shuralyov <shurcooL@gmail.com>
2014-10-21 20:06:49 -0700

    Improve code.google.com presenter to fetch changes when needed.
    If a given commit is not already available in remote vcsstore, try to
    clone or update the repo in vcsstore and try again.
    Include debug output.

--- presenter/codegoogle.go
+++ presenter/codegoogle.go
@@ -1,10 +1,13 @@
 package presenter
 
 import (
+	"bytes"
+	"fmt"
 	"html/template"
 	"net/url"
 	"strings"
 
+	"github.com/shurcooL/go-goon"
 	"github.com/shurcooL/go/gists/gist7480523"
 	"github.com/sourcegraph/go-vcs/vcs"
 	"github.com/sourcegraph/vcsstore/vcsclient"
@@ -106,9 +109,52 @@ func newCodeGoogleComparison(repo *gist7480523.GoPackageRepo) (c codeGoogleCompa
 		return
 	}
 
+	var debug = false
+	commitId, err := r.ResolveRevision(repo.GoPackages()[0].Dir.Repo.VcsRemote.RemoteRev)
+	if err != nil {
+		err1 := r.(vcsclient.RepositoryCloneUpdater).CloneOrUpdate(vcs.RemoteOpts{})
+		fmt.Println("newCodeGoogleComparison: CloneOrUpdate:", err1)
+		if err1 != nil {
+			c.err = MultiError{err, err1}
+			return
+		}
+		fmt.Println("newCodeGoogleComparison: DID NOT work on first try!!!")
+		goon.DumpExpr(repo.GoPackages()[0].Dir.Repo.VcsRemote.RemoteRev)
+		goon.DumpExpr(commitId)
+		goon.DumpExpr(err.Error())
+		goon.DumpExpr(err)
+		debug = true
+		commitId, err1 = r.ResolveRevision(repo.GoPackages()[0].Dir.Repo.VcsRemote.RemoteRev)
+		if err1 != nil {
+			c.err = MultiError{err, err1}
+			return
+		}
+		fmt.Println("newCodeGoogleComparison: did work on second try!!!!!!!!")
+		goon.DumpExpr(repo.GoPackages()[0].Dir.Repo.VcsRemote.RemoteRev)
+		goon.DumpExpr(commitId)
+	} else {
+		fmt.Println("newCodeGoogleComparison: worked on first try")
+	}
+
 	c.commits, _, c.err = r.Commits(vcs.CommitsOptions{
-		Head: vcs.CommitID(repo.GoPackages()[0].Dir.Repo.VcsRemote.RemoteRev),
+		Head: commitId,
 		N:    20, // Cap for now. TODO: Support arbtirary second revision to go until.
 	})
+	if debug {
+		goon.DumpExpr(c)
+	}
 	return
+}
+
+// ---
+
+type MultiError []error
+
+func (me MultiError) Error() string {
+	var buf bytes.Buffer
+	fmt.Fprintf(&buf, "%d errors:\n", len(me))
+	for _, err := range me {
+		fmt.Fprintln(&buf, err.Error())
+	}
+	return buf.String()
 }
