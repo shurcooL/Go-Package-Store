e294f927573ab24be168ea7733997bae61900dc9
Dmitri Shuralyov <shurcooL@gmail.com>
2015-01-29 18:31:04 -0800

    Add GA to estimate usage.
    This will allow me to have a better idea of how much GPS is used. If
    it's more popular, I can spend more time and effort to make it better,
    otherwise I can better allocate my limited resources on other projects.

--- main.go
+++ main.go
@@ -35,6 +35,16 @@ func CommonHat(w http.ResponseWriter) {
 		<link href="assets/style.css" rel="stylesheet" type="text/css" />
 		<script src="assets/script.js" type="text/javascript"></script>
 		<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/octicons/2.1.2/octicons.css">
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-56541369-2', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
 	</head>
 	<body>
 		<div style="width: 100%; text-align: center; background-color: hsl(209, 51%, 92%);">
