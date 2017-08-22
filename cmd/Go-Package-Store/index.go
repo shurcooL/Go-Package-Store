d87c0acc2366a355976c692fd30b1d091c5ea616
Dmitri Shuralyov <shurcooL@gmail.com>
2017-08-21 22:21:37 -0400

    cmd/Go-Package-Store: Use https scheme for GA URL.
    Using https scheme should always be okay, when /updates HTML page is
    served over http (most commonly, at localhost:8080) or https.
    
    This matches the default script that is provided by GA at this time.
    
    Fixes #83.

--- cmd/Go-Package-Store/index.go
+++ cmd/Go-Package-Store/index.go
@@ -17,7 +17,7 @@ var headHTML = template.Must(template.New("").Parse(`<html>
 		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 		  ga('create', 'UA-56541369-2', 'auto');
 		  ga('send', 'pageview');
